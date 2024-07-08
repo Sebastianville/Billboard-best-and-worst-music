fd6b6d054a56a27d5df39044ebde76add17ea163
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-07-08 14:08:29 -0400

    Worked on HTML by adding Month option

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function fetchMusic(){
-    return fetch("https://raw.githubusercontent.com/mhollingshead/billboard-hot-100/main/all.json")
-    .then(response => response.json())
-    .then(data => console.log(data));
-}
+    fetch("https://raw.githubusercontent.com/mhollingshead/billboard-hot-100/main/all.json")
+        .then(response => {
+         if (response.ok){
+            return response.json()
+        }else {
+            throw "We weren't able to obtain JSON"
+        };
+        })
+     .then(data => console.log(data));
+    //we need to probably do .forEach() to iterate the data 
+};
+
 fetchMusic();
 
+//We are going to need to creat an img, p, div, header 
+
+
+//inside the search bar(the week ending put a h5 tag and give the user an example as to how to use the serach bar): If...else if or do an forEach()
+
+//
+
+
+
+
+//Get the element that we need to attach it to "webpage"
 
 const h2 = document.createElement("h2");
 h2.textContent = "Billboard";
