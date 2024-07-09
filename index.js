8724f2147a8a6b6dfe1d71fdd87c7c1b457892ca
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-07-09 16:42:11 -0400

    worked on index and HTML

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const showFavoriteButton = document.getElementById("favoritemusic");
-const sideBar = document.getElementById("sidebar");
 const imgDisc = document.createElement("img");
-const h3 = document.getElementById("image"); //delete this later it was mostly to see what the image was going to render in the website 
 imgDisc.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/CD_autolev_crop_new.jpg/220px-CD_autolev_crop_new.jpg";
 imgDisc.style.height = "100px";
 imgDisc.style.width = "100px";
 
-h3.appendChild(imgDisc); //delete this later it was mostly to see what the image was going to render in the website 
+
+// const songDiv = document.getElementbyID("songBar");
+
+
 
 function fetchMusic(){
     fetch("https://raw.githubusercontent.com/mhollingshead/billboard-hot-100/main/all.json")
@@ -17,29 +17,36 @@ function fetchMusic(){
             throw "We weren't able to obtain JSON"
         };
         })
-     .then(data => console.log(data));
-    //we need to probably do .forEach() to iterate the data 
+     .then((data) => {
+        data.forEach((music) => {
+        renderMusicDetail(music)
+        renderMusicImg(music);
+     });
+});
 };
 
 fetchMusic();
 
 //We are going to need to creat an img, p, div, header 
 
-
-//inside the search bar(the week ending put a h5 tag and give the user an example as to how to use the serach bar): If...else if or do an forEach()
-
-//
-
-
+function renderMusicImg (music) {
+    let songDiv = document.getElementByID("songBar");
+    songDiv.append(imgDisc);
+}
 
 
-//Get the element that we need to attach it to "webpage"
-
-
-
-//event listener
-// showFavoriteButton.addEventListener('click', () => {
-//     sideBar.style.width = 20%
-//     ReportBody.style.marginRight = 20%
-// })
-
+function renderMusicDetail(music){
+    let songDetail = document.getElementById("songDetails");
+    let disc = document.getElementById("discs");
+    let artist = document.getElementById("artists");
+    let thisWeek = document.getElementById("thisWeek");
+    let lastWeek = document.getElementById("lastWeek");
+    let peakPosition = document.getElementById("peakPosition");
+   
+    songDetail.textContent = music.song;
+    thisWeek.textContent = music["this_week"];
+    lastWeek.textContent = music["last_week"];
+    artist.textContent = music.artist;
+    peakPosition.textContent = music["peak_position"];
+    disc.src = imgDisc;
+};
